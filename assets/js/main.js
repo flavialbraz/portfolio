62b149e932f942c5c37cb4cec50567d6d7a4ec10
Flávia Al. Braz <78583429+flavialbraz@users.noreply.github.com>
2022-04-20 20:46:48 -0300

    Commit inicial

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+ 
+const learningtecs = document.getElementById("learningtecs") 
+const tecsbtn = document.getElementById("tecs") 
+   
+tecsbtn.addEventListener ('click', () => {
+    learningtecs.classList.toggle('active')
+
+})
+ 
